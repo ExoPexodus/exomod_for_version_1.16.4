48765111e8d92783e1bb8ff6afc1d579e86b2b85
Rushil <rushilrana@gmail.com>
2023-04-19 03:10:55 +0530

    added a new item

--- src/main/java/net/Exo/pexomod/PexoMod.java
+++ src/main/java/net/Exo/pexomod/PexoMod.java
@@ -1,6 +1,7 @@
 package net.Exo.pexomod;
 
 import com.mojang.logging.LogUtils;
+import net.Exo.pexomod.item.Moditems;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -22,6 +23,8 @@ public class PexoMod
     public PexoMod()
     {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        Moditems.register(modEventBus);
 
         // Register the commonSetup method for modloading
         modEventBus.addListener(this::commonSetup);
